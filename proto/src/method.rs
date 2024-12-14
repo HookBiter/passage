e18b67eacd4ea1b529cedff76af7ca58659f1789
Hook Biter <hookbiter@gmail.com>
2024-12-14 02:36:34 +0200

    serde serialization testing

--- proto/src/method.rs
+++ proto/src/method.rs
@@ -1,5 +1,21 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
 pub enum Method {
     Connect,
     Disconnect,
     Pass,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::to_string;
+
+    #[test]
+    fn method_serialize_test() {
+        let m = Method::Connect;
+        let ser_m = to_string(&m);
+        assert_eq!(ser_m.unwrap(), "\"Connect\"");
+    }
 }
